eaac5c7cb6d4d8f0a3106f2d7d07b9541bc183fa
Andrew Mueller <mueller.andrewm@gmail.com>
2016-05-02 21:35:18 -0600

    Localization etc now actually properly work correctly. Also added USB intent and device filter.

--- app/src/main/java/li/bodhisattva/tangobotfinal/MainActivity.java
+++ app/src/main/java/li/bodhisattva/tangobotfinal/MainActivity.java
@@ -41,6 +41,33 @@ import com.google.atap.tangoservice.TangoConfig;
 import com.google.atap.tangoservice.TangoErrorException;
 import com.google.atap.tangoservice.TangoOutOfDateException;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoInvalidException;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity{
     private String TAG = "ATTN";
     private boolean usbPermission;
@@ -53,6 +80,9 @@ public class MainActivity extends AppCompatActivity{
     private UsbDeviceConnection connection;
     private UsbSerialPort sPort;
     private TextView mTitleTextView;
+    private Button theButton;
+    private Button goButton;
+    private Button testButton;
     private static final String ACTION_USB_PERMISSION =
             "com.android.example.USB_PERMISSION";
     private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
@@ -74,13 +104,43 @@ public class MainActivity extends AppCompatActivity{
             }
         }
     };
-
+    private boolean tangoPermissions = false;
+    private boolean mIsRelocalized;
+    private Object mSharedLock = new Object();
+    private TangoPoseData currentPose;
+    private TangoPoseData target;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.theButton = (Button) findViewById(R.id.lomarf);
+        this.goButton = (Button) findViewById(R.id.go_button);
+        this.testButton = (Button) findViewById(R.id.test_button);
         this.mTitleTextView = (TextView) findViewById(R.id.chome);
+        this.theButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mTitleTextView.setText("Setting target");
+                setTarget();
+            }
+        });
+        this.goButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mTitleTextView.setText("going to target");
+                go();
+            }
+        });
+        this.testButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mTitleTextView.setText("testing connection");
+                testConnection();
+            }
+        });
+        mTango = new Tango(this);
+
         startActivityForResult(
                 Tango.getRequestPermissionIntent(Tango.PERMISSIONTYPE_ADF_LOAD_SAVE),
                 Tango.TANGO_INTENT_ACTIVITYCODE);
@@ -94,14 +154,70 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
+    private void testConnection() {
+        sendCommand('w');
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sendCommand(' ');
+    }
+
+    private void go() {
+    }
+
+    private void setTarget() {
+        target = currentPose;
+    }
+
+
+
     @Override
     protected void onResume() {
+        mIsRelocalized = false;
         super.onResume();
+        if (tangoPermissions) {
+            // Clear the relocalization state: we don't know where the device has been since our app
+            // was paused.
+            mIsRelocalized = false;
+
+            // Re-attach listeners.
+            try {
+                setTangoListeners();
+            } catch (TangoErrorException e) {
+                Toast.makeText(getApplicationContext(), R.string.tango_error, Toast.LENGTH_SHORT)
+                        .show();
+            } catch (SecurityException e) {
+                Toast.makeText(getApplicationContext(), R.string.no_permissions, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            // Connect to the tango service (start receiving pose updates).
+            try {
+                mTango.connect(mConfig);
+            } catch (TangoOutOfDateException e) {
+                Toast.makeText(getApplicationContext(), R.string.tango_out_of_date_exception, Toast
+                        .LENGTH_SHORT).show();
+            } catch (TangoErrorException e) {
+                Toast.makeText(getApplicationContext(), R.string.tango_error, Toast.LENGTH_SHORT)
+                        .show();
+            } catch (TangoInvalidException e) {
+                Toast.makeText(getApplicationContext(), R.string.tango_invalid, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        try {
+            mTango.disconnect();
+        } catch (TangoErrorException e) {
+            Toast.makeText(getApplicationContext(), R.string.tango_error, Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 
     @Override
@@ -114,6 +230,7 @@ public class MainActivity extends AppCompatActivity{
         if (requestCode == Tango.TANGO_INTENT_ACTIVITYCODE) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
+                tangoPermissions = true;
                 Log.v("ATTN", "starting tango stuff");
                 mTango = new Tango(this);
                 mConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
@@ -181,25 +298,57 @@ public class MainActivity extends AppCompatActivity{
 
     private void setTangoListeners() {
         // Select coordinate frame pairs
-        ArrayList<TangoCoordinateFramePair> framePairs =
-                new ArrayList<TangoCoordinateFramePair>();
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
         framePairs.add(new TangoCoordinateFramePair(
                 TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
                 TangoPoseData.COORDINATE_FRAME_DEVICE));
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE));
+        Log.v(TAG, "Entering connectListener");
         mTango.connectListener(framePairs, new Tango.OnTangoUpdateListener() {
 
             @Override
             public void onPoseAvailable(TangoPoseData pose) {
-                if (pose.baseFrame == TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION
-                        && pose.targetFrame == TangoPoseData.COORDINATE_FRAME_DEVICE) {
-                    Log.v(TAG, "new ADF to device pose data");
-                } else if (pose.baseFrame == TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION
-                        && pose.targetFrame == TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE) {
-                    Log.v(TAG, "new localization");
-                }
-                Log.v(TAG, "lol get rekt scrub");
-            }
 
+                boolean updateRenderer = false;
+                // Make sure to have atomic access to Tango Data so that
+                // UI loop doesn't interfere while Pose call back is updating
+                // the data.
+                synchronized (mSharedLock) {
+                    // Check for Device wrt ADF pose, Device wrt Start of Service pose,
+                    // Start of Service wrt ADF pose (This pose determines if the device
+                    // is relocalized or not).
+                    if (pose.baseFrame == TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION
+                            && pose.targetFrame == TangoPoseData.COORDINATE_FRAME_DEVICE) {
+
+                        if (mIsRelocalized) {
+                            updateRenderer = true;
+                            currentPose = pose;
+                        }
+                    } else if (pose.baseFrame == TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE
+                            && pose.targetFrame == TangoPoseData.COORDINATE_FRAME_DEVICE) {
+                        if (!mIsRelocalized) {
+                            updateRenderer = true;
+                        }
+
+                    } else if (pose.baseFrame == TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION
+                            && pose.targetFrame == TangoPoseData
+                            .COORDINATE_FRAME_START_OF_SERVICE) {
+                        if (pose.statusCode == TangoPoseData.POSE_VALID) {
+                            mIsRelocalized = true;
+                            // Set the color to green
+                            currentPose = pose;
+                        } else {
+                            mIsRelocalized = false;
+                            // Set the color blue
+                        }
+                    }
+                }
+            }
             @Override
             public void onXyzIjAvailable(TangoXyzIjData tangoXyzIjData) {
 
